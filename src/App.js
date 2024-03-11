33ab723b290657a57414932b55045782c96b0927
Aygun Aghalarova <aygunaghalarova@Ayguns-MacBook-Pro.local>
2024-03-11 22:06:33 +0400

    caculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Button from './Components/Button';
+import './App.css'
 
 function App() {
+  let [num1, setNum1] = useState('');
+  let [num2, setNum2] = useState('');
+  let [operator, setOperator] = useState('');
+  let [result, setResult] = useState('');
+
+  const showResult = () => {
+    switch (operator) {
+      case "+":
+        setResult(parseInt(num1) + parseInt(num2))
+        break;
+      case "-":
+        setResult(parseInt(num1) - parseInt(num2))
+        break;
+      case "*":
+        setResult(parseInt(num1) * parseInt(num2))
+        break;
+      case "/":
+        setResult(parseInt(num1) / parseInt(num2))
+        break;
+
+      default:
+        break;
+    }
+    setNum1('');
+    setNum2('');
+    setOperator('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="caculator">
+      <input type='text' placeholder='Num1' value={num1} onChange={(e) => { setNum1(e.target.value) }} />
+      <input type='text' placeholder='Num2' value={num2} onChange={(e) => { setNum2(e.target.value) }} />
+      <input type='text' placeholder='+, -, *, /' value={operator} onChange={(e) => { setOperator(e.target.value) }} />
+      <Button onClick={showResult} >=</Button>
+      <p>Result: {result}</p>
     </div>
   );
 }
---
+++ src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import styles from './Button.module.css'
+
+const Button = ({onClick,children}) => {
+    
+  return (
+    <div>
+        <button onClick={onClick} className={styles.button}>{children}</button>
+    </div>
+  )
+}
+
+export default Button
